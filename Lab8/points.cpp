81184a179a9255ba9c673df1dfa66f4daf33c723
Niraj Kumar <nirajk23523@gmail.com>
2023-03-07 16:11:22 +0530

    started Lab 8

---
+++ Lab8/points.cpp
@@ -0,0 +1,13 @@
+#include "points.h"
+
+Point::Point(int x, int y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+Point::Point()
+{
+    this->x = 0;
+    this->y = 0;
+}
---
+++ Lab8/points.h
@@ -0,0 +1,12 @@
+#ifndef POINTS_H
+#define POINTS_H
+
+class Point
+{
+public:
+    Point(int x, int y);
+    Point();
+    int x;
+    int y;
+};
+#endif

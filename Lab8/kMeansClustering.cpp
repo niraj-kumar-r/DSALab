0589f8245625e77ab6581c3c403aa2cd58d29948
Niraj Kumar <nirajk23523@gmail.com>
2023-04-17 00:46:46 +0530

    doKmeans

--- Lab8/kMeansClustering.cpp
+++ Lab8/kMeansClustering.cpp
@@ -1,5 +1,6 @@
 #include "kMeansClustering.h"
 #include <iostream>
+#include <unordered_set>
 using namespace std;
 
 cluster::cluster()
@@ -76,9 +77,20 @@ void my_graph::setPoints(std::vector<GraphPoint> points)
 
 void my_graph::assignRandomCentroids()
 {
+    unordered_set<int> used;
     for (int i = 0; i < numClusters; i++)
     {
-        clusters[i] = cluster(graphPoints[i]);
+        while (true)
+        {
+            int randNum = rand() % numPoints;
+            if (used.find(randNum) == used.end())
+            {
+                used.insert(randNum);
+                clusters[i] = cluster(graphPoints[randNum]);
+                graphPoints[randNum].cluster = i;
+                break;
+            }
+        }
     }
 }
 
@@ -98,5 +110,56 @@ void my_graph::removePoint(GraphPoint p)
             numPoints--;
             return;
         }
+    }
+}
+
+void my_graph::doKMeans()
+{
+    for (int i = 0; i < numClusters; i++)
+    {
+        clusters[i].clearTree();
+    }
+    for (auto &p : graphPoints)
+    {
+        p.cluster = NULL;
+    }
+    assignRandomCentroids();
+    bool changed = true;
+    while (changed)
+    {
+        changed = false;
+        for (int i = 0; i < numPoints; i++)
+        {
+            int minCluster = 0;
+            double minDist = graphPoints[i].calcDist(clusters[0].centroid);
+            for (int j = 1; j < numClusters; j++)
+            {
+                double dist = graphPoints[i].calcDist(clusters[j].centroid);
+                if (dist < minDist)
+                {
+                    minDist = dist;
+                    minCluster = j;
+                }
+            }
+            if (graphPoints[i].cluster != minCluster)
+            {
+                changed = true;
+                if (graphPoints[i].cluster != NULL)
+                {
+                    clusters[graphPoints[i].cluster].remove(graphPoints[i]);
+                }
+                graphPoints[i].cluster = minCluster;
+                clusters[minCluster].insert(graphPoints[i]);
+            }
+        }
+        for (int i = 0; i < numClusters; i++)
+        {
+            clusters[i].updateCentroid();
+        }
+    }
+    for (int i = 0; i < numClusters; i++)
+    {
+        cout << "Cluster " << i << " Centroid: " << clusters[i].centroid.x << ", " << clusters[i].centroid.y << endl;
+        clusters[i].print();
     }
 }

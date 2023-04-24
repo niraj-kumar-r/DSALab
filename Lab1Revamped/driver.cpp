67c9a8d93484c3c61bcfdbfcb8999a699eb5f6a7
Niraj Kumar <nirajk23523@gmail.com>
2023-04-25 01:02:19 +0530

    update

--- Lab1Revamped/driver.cpp
+++ Lab1Revamped/driver.cpp
@@ -3,23 +3,6 @@
 
 using namespace std;
 
-// int main()
-// {
-//     int numEntries = 10;
-//     int values[] = {1, 2, 3, 4, 5};
-//     float weights[] = {0.1, 4.2, 2.3, 0.4, 5.5};
-
-//     weightedMedianSolver solver = weightedMedianSolver();
-
-//     for (int i = 0; i < numEntries; i++)
-//     {
-//         solver.insert(values[i], weights[i]);
-//     }
-//     cout << "Median: " << solver.getMedian() << endl;
-// }
-
-// write menu based program to take input, show median, and remove value and exit
-
 int main()
 {
     weightedMedianSolver solver = weightedMedianSolver();

7ee439358fd23270c485fcf90cb5dae99829c71e
Niraj Kumar <nirajk23523@gmail.com>
2023-04-25 12:39:50 +0530

    menu based

--- Lab10/driver.cpp
+++ Lab10/driver.cpp
@@ -6,12 +6,54 @@ using namespace std;
 int main()
 {
     regToNFAConvertor *regToNFA = new regToNFAConvertor();
-    string reg = "a+(b*.c)*+d";
-    NFAGraph *nfa = regToNFA->regToNFA(reg);
-    cout << nfa->isValidString("abd") << endl;
-    cout << nfa->isValidString("acd") << endl;
-    cout << nfa->isValidString("ac") << endl;
-    cout << nfa->isValidString("bc") << endl;
-    cout << nfa->isValidString("bbc") << endl;
-    cout << nfa->isValidString("bbcbbbc") << endl;
+    string reg;
+    NFAGraph *nfa = nullptr;
+    string str;
+    int choice;
+    bool result;
+    while (true)
+    {
+        cout << endl;
+        cout << "Hello, Welcome to the NFA simulator\n"
+             << endl;
+        cout << "Present Regex: " << reg << endl;
+        cout << "Choices: " << endl;
+        cout << "1. Enter a regular expression" << endl;
+        cout << "2. Check if a string is valid" << endl;
+        cout << "3. Exit\n"
+             << endl;
+        cout << "Enter your choice: ";
+        cout << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the regular expression: ";
+            cin >> reg;
+            nfa = regToNFA->regToNFA(reg);
+            break;
+        case 2:
+            if (nfa == nullptr)
+            {
+                cout << "Please enter a regular expression first" << endl;
+                break;
+            }
+            cout << "Enter the string: ";
+            cin >> str;
+            result = nfa->isValidString(str);
+            if (result)
+            {
+                cout << "Accepted :)" << endl;
+            }
+            else
+            {
+                cout << "Rejected :(" << endl;
+            }
+            break;
+        case 3:
+            return 0;
+        default:
+            cout << "Invalid choice" << endl;
+        }
+    }
 }

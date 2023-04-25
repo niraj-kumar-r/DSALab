4ec8e6b38378d510ce470f90512fe1745824c545
Niraj Kumar <nirajk23523@gmail.com>
2023-04-25 12:51:15 +0530

    perfect hash

--- Lab9/main.cpp
+++ Lab9/main.cpp
@@ -3,18 +3,48 @@
 #include "hash_table.h"
 using namespace std;
 
-int main() {
-  cout << "Welcome to the perfect hash table!" << endl;
-  cout << "Provide the #elements followed by the elements:"<<endl;
+int main()
+{
+  cout << "\nWelcome to the perfect hash table!" << endl;
   hash_table h;
-  int n;
-  cin>>n;
-  for (int i=0; i<n; i++){
-    int j;
-    cin>>j;
-    h.insert(j);
-    cout<<"Inserted "<<j<<endl;
+  int choice;
+  while (true)
+  {
+    cout << "\nChoices:\n";
+    cout << "1. Insert\n";
+    cout << "2. Delete\n";
+    cout << "3. Print\n";
+    cout << "4. Exit\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+      cout << "Enter the #elements followed by the elements:" << endl;
+      int n;
+      cin >> n;
+      for (int i = 0; i < n; i++)
+      {
+        int j;
+        cin >> j;
+        h.insert(j);
+        cout << "Inserted " << j << endl;
+      }
+      break;
+    case 2:
+      cout << "Enter the element to delete: ";
+      cin >> n;
+      h.Delete(n);
+      cout << "Deleted " << n << endl;
+      break;
+    case 3:
+      cout << "\nPrinting the hash table:\n";
+      h.print();
+      break;
+    case 4:
+      return 0;
+    default:
+      cout << "Invalid choice" << endl;
+    }
   }
-  h.print();
-  return 0;
 }
